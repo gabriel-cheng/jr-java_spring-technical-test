664ba234d0718a182c5e76a8f1abb5b87da4e2ee
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-03-24 12:06:14 -0300

    feat: ending vehicle controller

--- src/main/java/com/example/technicaltest/controller/VehicleController.java
+++ src/main/java/com/example/technicaltest/controller/VehicleController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +39,17 @@ public class VehicleController {
         return ResponseEntity.ok(allVehicles);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Vehicle>> getOneVehicle(@PathVariable String id) {
+        Optional<Vehicle> vehicleFinded = vehicleRepository.findById(id);
+
+        if(!vehicleFinded.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(vehicleFinded, HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<String> registerNewVehicle(@RequestBody @Validated RequestVehicle vehicle) {
         Optional<Person> personFinded = personRepository.findById(vehicle.personid());
@@ -50,5 +64,43 @@ public class VehicleController {
         vehicleRepository.save(newVehicle);
 
         return new ResponseEntity<>("New vehicle added successfully!", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> changeVehicle(@PathVariable @Validated String id, @RequestBody RequestVehicle data) {
+        Optional<Vehicle> vehicleFinded = vehicleRepository.findById(id);
+        Optional<Person> personFinded = personRepository.findById(data.personid());
+
+        if(!vehicleFinded.isPresent()) {
+            return new ResponseEntity<>("Vehicle not found!", HttpStatus.NOT_FOUND);
+        }
+        if(!personFinded.isPresent()) {
+            return new ResponseEntity<>("Person not found!", HttpStatus.NOT_FOUND);
+        }
+
+        Vehicle vehicle = vehicleFinded.get();
+        vehicle.setBrand(data.brand());
+        vehicle.setModel(data.model());
+        vehicle.setColor(data.color());
+        vehicle.setPlate(data.plate());
+        vehicle.setPrice(data.price());
+        vehicle.setPerson(personFinded.get());
+
+        vehicleRepository.save(vehicle);
+
+        return new ResponseEntity<>("Changes applied successfully!", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteVehicle(@PathVariable String id) {
+        Optional<Vehicle> vehicleFinded = vehicleRepository.findById(id);
+
+        if(!vehicleFinded.isPresent()) {
+            return new ResponseEntity<>("Vehicle not found!", HttpStatus.NOT_FOUND);
+        }
+
+        vehicleRepository.deleteById(id);
+
+        return new ResponseEntity<>("Vehicle deleted successfully!", HttpStatus.ACCEPTED);
     }
 }
