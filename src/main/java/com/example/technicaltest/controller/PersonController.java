4f436a8827a7f518653547cd4ae40692ef692b4f
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-03-24 12:20:37 -0300

    feat: adding PUT method

--- src/main/java/com/example/technicaltest/controller/PersonController.java
+++ src/main/java/com/example/technicaltest/controller/PersonController.java
@@ -3,14 +3,18 @@ package com.example.technicaltest.controller;
 import java.util.List;
 import java.util.Optional;
 import com.example.technicaltest.domain.vehicle.VehicleRepository;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.aspectj.weaver.patterns.PerObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,6 +48,10 @@ public class PersonController {
     public ResponseEntity<Optional<Person>> getOnePerson(@PathVariable String id) {
         Optional<Person> personFinded = personRepository.findById(id);
 
+        if(!personFinded.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return ResponseEntity.ok(personFinded);
     }
 
@@ -53,19 +61,40 @@ public class PersonController {
 
         personRepository.save(newPerson);
 
-        return ResponseEntity.ok("New person created!");
+        return new ResponseEntity<>("New person created!", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> changePerson(@RequestBody @Validated RequestPerson data, @PathVariable String id) {
+        Optional<Person> personFinded = personRepository.findById(id);
+
+        if(!personFinded.isPresent()) {
+            return new ResponseEntity<>("Person not found!", HttpStatus.NOT_FOUND);
+        }
+
+        Person newPerson = personFinded.get();
+        newPerson.setName(data.name());
+        newPerson.setCpf(data.cpf());
+        newPerson.setAge(data.age());
+        newPerson.setGenre(data.genre());
+        newPerson.setEmail(data.email());
+        newPerson.setCellphone(data.cellphone());
+
+        personRepository.save(newPerson);
+
+        return new ResponseEntity<>("Person changed successfully!", HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePerson(@PathVariable String id) {
         Optional<Person> personToDelete = personRepository.findById(id);
 
-        if(personToDelete.isPresent()) {
-            personRepository.deleteById(id);
+        if(!personToDelete.isPresent()) {
+            return new ResponseEntity<>("Person not found!", HttpStatus.NOT_FOUND);
+        }
+        
+        personRepository.deleteById(id);
 
-            return ResponseEntity.ok("Person deleted successfully!");
-        } 
-
-        return ResponseEntity.ok("Person not found!");
+        return ResponseEntity.ok("Person deleted successfully!");
     }
 }
